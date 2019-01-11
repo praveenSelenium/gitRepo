cd763d603398c44ce9c42861620138c16c988a01
praveenSelenium <praveen.mamidi@styleresearch.com>
2019-01-11 11:22:07 +0000

    etetet

---
+++ src/newpush.java
@@ -0,0 +1,9 @@
+public class newpush {
+
+
+
+    public void newpush(){
+
+        System.out.println("pushing new code");
+    }
+}
---
+++ src/samplepush.java
@@ -0,0 +1,8 @@
+public class samplepush {
+
+
+    public void samplepush(){
+
+        System.out.println("pushing code from branch to mster");
+    }
+}
